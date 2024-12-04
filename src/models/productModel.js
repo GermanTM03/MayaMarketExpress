fd0010bc12d21c93458f870b777482dc1df60c84
GermanTM03 <126848184+GermanTM03@users.noreply.github.com>
2024-12-04 01:48:33 -0500

    Correccion

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema({
   image_1: { type: String, required: true },
   image_2: { type: String },
   image_3: { type: String },
-  categories: [{ type: String, required: false }],
+  categories: { type: String, required: false },
   stock: { type: Number, required: true, min: 0 },
   price: { type: Number, required: true, min: 0 },
   quantity: { type: Number, required: true, min: 0 },
